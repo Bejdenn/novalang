b4d1317d72d26e7bee5a8d3b863b6e6d271af239
Dennis <53354166+Bejdenn@users.noreply.github.com>
2024-02-01 18:57:31 +0100

    Tests can put in folders that represent the expected exit code; add tests for symbol handling

--- tests.sh
+++ tests.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-for f in $(find ./tests/ -name "*.nva" -type f | sort); do
+echo "Tests that expect to have a successful exit code"
+for f in $(find ./tests/expect/success -name "*.nva" -type f | sort); do
     out=$(cat $f | ./novalang 2>&1 1>/dev/null)
 
     if [ $? -eq 0 ]; then
         echo "\e[32m$f\e[0m"
     else
         echo "\e[31m$f\e[0m" " " $out
+    fi
+done
+
+echo
+
+echo "Tests that expect to have a non-zero exit code"
+for f in $(find ./tests/expect/error -name "*.nva" -type f | sort); do
+    out=$(cat $f | ./novalang 2>&1 1>/dev/null)
+
+    if [ $? -ne 0 ]; then
+        echo "\e[32m$f\e[0m" "\t" $out
+    else
+        echo "\e[31m$f\e[0m"
     fi
 done
