78e78aba1c7a3b4a3f133f6ad5c7034b267a827a
Dennis <53354166+Bejdenn@users.noreply.github.com>
2024-02-02 18:41:31 +0100

    Tests can be set up to expect output or supply input; fix format of test results

--- tests.sh
+++ tests.sh
@@ -1,27 +1,60 @@
 #!/bin/bash
 
-echo "Tests that expect to have a successful exit code"
-for f in $(find ./tests/expect/success -name "*.nva" -type f | sort); do
+success() {
+    echo " " "\e[32m$1\e[0m"
+}
+
+error() {
+    echo " " "\e[31m$1\e[0m"
+    echo "  "$2
+}
+
+echo "Test: zero exit code"
+for f in $(find tests/expect/success -maxdepth 1 -name "*.nva" -type f | sort); do
     out=$(./novalang $f 2>&1 1>/dev/null)
 
     if [ $? -eq 0 ]; then
-        echo "\e[32m$f\e[0m"
+        success $f
     else
-        echo "\e[31m$f\e[0m"
-        echo "  " $out
+        error $f $out
     fi
 done
 
 echo
 
-echo "Tests that expect to have a non-zero exit code"
-for f in $(find ./tests/expect/error -name "*.nva" -type f | sort); do
+echo "Test: zero exit code + correct output"
+for f in $(find tests/expect/success/on-output -maxdepth 1 -name "*.nva" -type f | sort); do
+    out=$(./novalang $f | diff ${f%.nva}.output -)
+
+    if [ $? -eq 0 ]; then
+        success $f
+    else
+        error $f $out
+    fi
+done
+
+echo
+
+echo "Test: zero exit code + correct output for input"
+for f in $(find tests/expect/success/on-output/with-input -maxdepth 1 -name "*.nva" -type f | sort); do
+    out=$(./novalang $f <${f%.nva}.input | diff ${f%.nva}.output -)
+
+    if [ $? -eq 0 ]; then
+        success $f
+    else
+        error $f $out
+    fi
+done
+
+echo
+
+echo "Test: non-zero exit code"
+for f in $(find tests/expect/error -name "*.nva" -type f | sort); do
     out=$(./novalang $f 2>&1 1>/dev/null)
 
     if [ $? -ne 0 ]; then
-        echo "\e[32m$f\e[0m"
-        echo "  " $out
+        success $f
     else
-        echo "\e[31m$f\e[0m"
+        error $f
     fi
 done
