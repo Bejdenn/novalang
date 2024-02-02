fbcb8ddc292dc25efbf2f552a3767a5921779c20
Dennis <53354166+Bejdenn@users.noreply.github.com>
2024-02-02 16:20:12 +0100

    Print test output on separate line

--- tests.sh
+++ tests.sh
@@ -7,7 +7,8 @@ for f in $(find ./tests/expect/success -name "*.nva" -type f | sort); do
     if [ $? -eq 0 ]; then
         echo "\e[32m$f\e[0m"
     else
-        echo "\e[31m$f\e[0m" " " $out
+        echo "\e[31m$f\e[0m"
+        echo "  " $out
     fi
 done
 
@@ -18,7 +19,8 @@ for f in $(find ./tests/expect/error -name "*.nva" -type f | sort); do
     out=$(cat $f | ./novalang 2>&1 1>/dev/null)
 
     if [ $? -ne 0 ]; then
-        echo "\e[32m$f\e[0m" "\t" $out
+        echo "\e[32m$f\e[0m"
+        echo "  " $out
     else
         echo "\e[31m$f\e[0m"
     fi
